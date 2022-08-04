99bc5edb338e4fb7cd8aacb6d8622071d18925eb
David <mail.davedev@gmail.com>
2022-08-04 12:53:17 +0200

    react integration in django

--- Appathon-Project/disabilityhub/disabilityhub/asgi.py
+++ Appathon-Project/disabilityhub/disabilityhub/asgi.py
@@ -11,7 +11,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chatapi.routing
+import frontend.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disabilityhub.settings')
 
@@ -19,7 +19,7 @@ application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
     URLRouter(
-      chatapi.routing.websocket_urlpatterns
+      frontend.routing.websocket_urlpatterns
     )
   )
 })
--- Appathon-Project/disabilityhub/frontend/views.py
+++ Appathon-Project/disabilityhub/frontend/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from .models import Message
 
 def index(request):
-    return render(request, 'chatapi/index.html')
+    return render(request, 'frontend/index.html')
 
 def room(request, room_name):
     username = request.GET.get('username', 'Anonymous')
     messages = Message.objects.filter(room=room_name)[0:25]
 
-    return render(request, 'chatapi/room.html', {'room_name': room_name, 'username': username, 'messages': messages})
+    return render(request, 'frontend/room.html', {'room_name': room_name, 'username': username, 'messages': messages})
