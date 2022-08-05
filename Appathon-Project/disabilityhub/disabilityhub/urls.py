c6b01a54347c7c801914e677833185b5c79a560a
Jonas Wolf <it.wolfx64@gmail.com>
2022-08-05 16:10:08 +0200

    changed room url pattern to :/room/<room_name>

--- Appathon-Project/disabilityhub/disabilityhub/urls.py
+++ Appathon-Project/disabilityhub/disabilityhub/urls.py
@@ -20,5 +20,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('frontend.urls')),
+    path('room/', include('frontend.urls')),
 ]
