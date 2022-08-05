dd5a36ea8ebf6e5a0704626b1d67248c0435fd96
Jonas Wolf <it.wolfx64@gmail.com>
2022-08-05 23:26:43 +0200

    added support for usernames and guestnames

--- speakable/frontend/views.py
+++ speakable/frontend/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-
+from random import randint
 from .models import Message
 
 def index(request):
     return render(request, 'frontend/index.html')
 
 def room(request, room_name):
-    username = request.GET.get('username', 'Anonymous')
     messages = Message.objects.filter(room=room_name)[0:25]
+
+    if request.user.is_authenticated:
+        # use existing username
+        username = request.user.username
+    else:
+        # generate random guest username
+        username = 'guest' + ''.join([str(randint(0, 9)) for i in range(8)])
 
     return render(request, 'frontend/room.html', {'room_name': room_name, 'username': username, 'messages': messages})
