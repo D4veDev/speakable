fe0dceb788cc8484863c237cdb6b4fd42fd820e0
Jonas Wolf <it.wolfx64@gmail.com>
2022-08-05 16:50:57 +0200

    added signup/login/logout and account management

--- Appathon-Project/disabilityhub/disabilityhub/urls.py
+++ Appathon-Project/disabilityhub/disabilityhub/urls.py
@@ -20,5 +20,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('room/', include('frontend.urls')),
 ]
--- Appathon-Project/disabilityhub/frontend/urls.py
+++ Appathon-Project/disabilityhub/frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'frontend'
+
 urlpatterns = [
   path('', views.index, name='index'),
   path('<str:room_name>/', views.room, name='room')
