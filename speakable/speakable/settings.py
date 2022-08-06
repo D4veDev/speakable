4c56af269b5b6dc2fbd243cd2282c764230483c1
David <mail.davedev@gmail.com>
2022-08-06 09:10:25 +0100

    worked on base.html

--- speakable/speakable/settings.py
+++ speakable/speakable/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,5 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = str(BASE_DIR / 'static') + '/'
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
 
 # Other
